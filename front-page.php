3ac623b02968bc2ee882e75767001b1fcf290de0
Nome Q <dickerson.nomi@gmail.com>
2019-01-22 13:03:15 -0800

    refine new contact form

--- front-page.php
+++ front-page.php
@@ -77,6 +77,7 @@
 		<!-- Begin Constant Contact Inline Form Code -->
 		<div class="ctct-inline-form" data-form-id="e054f108-477a-415e-b5cf-18dc8b97e2cd"></div>
 		<!-- End Constant Contact Inline Form Code -->
+		<div class="privacy-policy"><a href="/contact-form-privacy-policy">Privacy Policy</a></div>
 	</div>
 
 </div> <!-- end home-secondary-container -->
