4ae2c24bbb752bc37351a838aafa226e03c24ba5
Nome Q <dickerson.nomi@gmail.com>
2019-01-22 11:44:54 -0800

    add constant contact form

--- footer.php
+++ footer.php
@@ -1,56 +1,60 @@
 	</div>
 </div> <!-- end #content -->
-			
+
 <footer class="footer" role="contentinfo">
 	<div id="inner-footer" class="wrap cf">
-		
+
 		<div id="footer-about">
-			
-			<?php 
-		
+
+			<?php
+
 				$about = get_page_by_path( 'about-mountain-transit', OBJECT );
-				
+
 				if ( isset($about) ) {
 					$excerpt = get_extended( $about->post_content )['main'];
-					echo $excerpt . ' '; 
+					echo $excerpt . ' ';
 					printf( '<a href="%s">Read more about Mountain Transit &raquo;</a>', get_permalink($about->ID) );
 					edit_post_link( 'Edit this text', '<br />', '', $about->ID );
 				}
 			?>
-			
+
 		</div>
 
 		<div id="footer-left-menu">
-	
+
 				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'depth' => 1 ) ); ?>
-		
+
 		</div>
-		
+
 		<div id="footer-right-menu" class="clear">
-			
+
 			<?php wp_nav_menu( array( 'theme_location' => 'footer-secondary', 'depth' => 1 ) ); ?>
-			
+
 		</div>
-		
+
 		<br style="clear:both" />
-		
+
 		<div id="footer-copyright" class="source-org">
-			
+
 			&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>
-			
+
 		</div>
-		
+
 		<div id="footer-attributions">
-			
+
 			<a href="<?php echo get_site_url(); ?>/site-credits">Site Credits</a>
-			
+
 		</div>
-			
+
 	</div>
 
 </footer>
 
 </div> <!-- end #container -->
+<!-- Begin Constant Contact Active Forms -->
+<script> var _ctct_m = "d1555e2923350a0778b93bd6a2c6ab54"; </script>
+<script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
+<!-- End Constant Contact Active Forms -->
 <?php wp_footer(); ?>
 </body>
 </html>
--- front-page.php
+++ front-page.php
@@ -74,18 +74,9 @@
 	</div> <!-- end home-news-area -->
 
 	<div id="newsletter-subscribe">
-		<h3>Sign up to receive news and alert updates</h3>
-		<div id="mc_embed_signup">
-		<form action="https://mountaintransit.us17.list-manage.com/subscribe/post?u=865de37491ec5dc5d1b217746&amp;id=4f238d188d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-		    <div id="mc_embed_signup_scroll" class="form-row">
-			<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-		    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-		    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_865de37491ec5dc5d1b217746_4f238d188d" tabindex="-1" value=""></div>
-		    <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">Subscribe</button>
-		    </div>
-		</form>
-		</div>
-		<!--End mc_embed_signup-->
+		<!-- Begin Constant Contact Inline Form Code -->
+		<div class="ctct-inline-form" data-form-id="e054f108-477a-415e-b5cf-18dc8b97e2cd"></div>
+		<!-- End Constant Contact Inline Form Code -->
 	</div>
 
 </div> <!-- end home-secondary-container -->
